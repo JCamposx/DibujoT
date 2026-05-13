fb8d2a8ac4c38b51f19a1aef155f0a11378b49ca
Jesús Campos <jesus.campos123@icloud.com>
2026-05-13 17:22:42 -0500

    feat(serial): add SerialPort interface, UsbSerialPort and FakeSerialPort

---
+++ app/src/main/java/com/example/dibujot/serial/SerialPort.kt
@@ -0,0 +1,9 @@
+package com.example.dibujot.serial
+
+interface SerialPort : java.io.Closeable {
+    fun connect(baudRate: Int = 115200)
+    fun send(line: String)
+    fun readLine(): String?
+    fun disconnect()
+    override fun close() = disconnect()
+}
